e672bac653dbb6e8d9c893a5701d9cf47fa6fb21
Netossim <120585455+Netossim@users.noreply.github.com>
2024-03-11 14:29:09 -0300

    Update Principal.java

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -3,7 +3,19 @@
  * Veja o README para mais detalhes.
  */
 public class Principal {
-    public static void main(final String[] args) {
+       public static void main(String[] args) {
+        // Testando as classes e records
+        CidadeRecord cidadeRecord = new CidadeRecord("Tocantins");
+        Cidade cidade = new Cidade("Brasília");
 
+        PessoaRecordShallow pessoaShallow = new PessoaRecordShallow("Fulano", cidade);
+        PessoaRecord pessoa = new PessoaRecord("Ciclano", cidadeRecord);
+
+        // Exibindo informações
+        System.out.println("CidadeRecord: " + cidadeRecord);
+        System.out.println("Cidade: " + cidade.getNome());
+        System.out.println("PessoaRecordShallow: " + pessoaShallow);
+        System.out.println("PessoaRecord: " + pessoa);
     }
+
 }
